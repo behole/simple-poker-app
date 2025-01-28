a03980e00e8ccf0a87d8eef7d435e3e3dde12af2
behole <behole@gmail.com>
2025-01-27 16:25:31 -0800

    Add App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PokerGame from './components/PokerGame';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-background p-4">
+      <PokerGame />
+    </div>
+  );
+}
+
+export default App;
